b408545bbfcb87a645f2dd7f1902f4922d85dba8
jbakambana <jey@aims.ac.za>
2024-04-19 09:17:55 -0400

    Create run_cog_cluster.sh

---
+++ run_cog_cluster.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --mem=16G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --time=5-3:0:0
+#SBATCH --mail-user=jeremie.bakambana@umontreal.ca
+#SBATCH --mail-type=ALL
+#SBATCH --gres=gpu:v100l:1
+
+
+module purge
+module load python/3.9 cuda cudnn scipy-stack
+source ~/py39/bin/activate
+
+python /home/jey/projects/def-sponsor00/jey/cog/examples/cog.py \
+     --env=Widow250PickTray-v0 \
+     --max-path-length=40 \
+     --prior-buffer=pickplace_prior.npy \
+     --task-buffer=pickplace_task.npy
